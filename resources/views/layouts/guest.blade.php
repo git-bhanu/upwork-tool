f1ce06d8097977256db98f33df9b92452987ac15
Bhanu <bhanu@krenovate.com>
2021-08-17 19:45:59 +0530

    Fixed versioning issue in mix

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,10 +13,10 @@
 
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body>
         <x-validation></x-validation>
