9559359ef2d41827d90c5fad1e49e2da5860ed4c
Bhanu <bhanu@krenovate.com>
2021-08-27 11:45:26 +0530

    Added right name component

--- resources/views/components/single-user-comment.blade.php
+++ resources/views/components/single-user-comment.blade.php
@@ -4,7 +4,9 @@
     </div>
     <div class="w-full">
     <div class="text-gray-600 bg-gray-100 ml-4 px-5 py-2 pb-3 rounded-2xl rounded-tl-none w-4/5 flex-column">
-        <span class="text-xs text-gray-900 font-bold">{{ $name }}</span>
+        <span class="text-xs text-gray-900 font-bold">
+            <x-user-name :user="$user"/>
+        </span>
         <p>{{ $comment->comment }}</p>
         <p class="text-xs text-right text-gray-400 mt-5"> {{ $comment_time }} </p>
     </div>
