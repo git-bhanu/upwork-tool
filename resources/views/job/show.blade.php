aa9845fd6a95c9354c5596086bfb34cd3afc4829
Bhanu <bhanu@krenovate.com>
2021-08-27 11:41:40 +0530

    Job area made responsive.

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -9,13 +9,13 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg px-8 py-8 flex">
-                <div class="w-2/3 border-gray-200 pr-6">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg px-8 py-8 flex flex-col-reverse md:flex-row md:flex-nowrap">
+                <div class="border-gray-200 pr-6 w-full md:w-2/3  mt-6 md:mt-0">
                     @include('job.basic-detail', ['job' => $job])
                     @livewire('comment', ['job_id' => $job->id])
 
                 </div>
-                <div class="w-1/3">
+                <div class="w-full md:w-1/3">
                     @livewire('job-actions', ['job_id' => $job->id])
 
                     @hasanyrole('sales-manager|super-admin')
